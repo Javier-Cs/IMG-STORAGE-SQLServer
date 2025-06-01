fa769d81a9e0b0ebbee5758897b5fd2b2b322183
Javier-Cs <krchipullajavier@gmail.com>
2025-06-01 11:30:31 -0500

    Implementación de dependencias de cloudinary en pom.xml

---
+++ src/main/java/com/example/StorageOpen/StorageOpenApplication.java
@@ -0,0 +1,13 @@
+package com.example.StorageOpen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageOpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageOpenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StorageOpen/StorageOpenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StorageOpen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageOpenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
